3dc8d9e16c4e020b946b98ffd5aba1b96079ee13
jhonab95 <141082832+jhonab95@users.noreply.github.com>
2023-11-29 22:40:20 -0300

    Solucionar problema de propagación de eventos

--- eventos.js
+++ eventos.js
@@ -2,7 +2,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     var contenedorBoton = document.getElementById("contenedorBoton");
 
-    contenedorBoton.addEventListener("click", function () {
+    contenedorBoton.addEventListener("click", function (event) {
         alert("¡Hola! Soy el div");
+        event.stopPropagation(); // Detiene la propagación del evento
+    });
+
+    document.getElementById("saludarBtn").addEventListener("click", function (event) {
+        alert("¡Hola desde eventos.js!");
+        event.stopPropagation(); // Detiene la propagación del evento
     });
 });
